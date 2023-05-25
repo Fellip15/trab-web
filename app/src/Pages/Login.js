5eb453c33dc52adaf9e6514a7d1b9514d1f027cc
Fellip15 <fellipalves15@gmail.com>
2023-05-25 12:22:28 -0300

    Natal ta mexendo no codigo =

--- app/src/Pages/Login.js
+++ app/src/Pages/Login.js
@@ -6,36 +6,86 @@ import "./css/Login.css"
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 
-const Login = () => {
+const Login = ({ dataUsers }) => {
 
     const navigate = useNavigate();
     const handleRegister = () => {
         navigate(`/register`);
     }
 
+    // atribui os valores do input a cada variável referente
+    const [inputEmail, setInputEmail] = useState('');
+    const handleInputEmailChange = (event) => {
+        setInputEmail(event.target.value);
+    };
+
+    const [inputPassword, setInputPassword] = useState('');
+    const handleInputPasswordChange = (event) => {
+        setInputPassword(event.target.value);
+    };
+
+    // verifica o login nos dados e direciona à página do usuário
+    const handleClickLogin = () => {
+        // filtra os usuários no banco com email e senha lidos
+        const targetUser = dataUsers.filter(user => 
+            user.email.toLowerCase() === inputEmail.toLowerCase() &&
+            user.password === inputPassword
+        );
+        
+        // verifica se algum usuário possui email e senha definidos
+        if (targetUser.lenght !== 1) {
+            alert('Email ou senha inválidos');
+        }
+        else {
+            
+        }
+
+        // reseta os valores de input
+        setInputEmail('');
+        setInputPassword('');
+    };
+
     return (
         <>
             <Header />
-
-            <div class="login-frame content">
-                <div class="error-message error-message-hide">
-                    <p class="error-message-text">
+            
+            <div className="login-frame content">
+                <div className="error-message error-message-hide">
+                    <p className="error-message-text">
                         Possível mensagem de erro, errou a senha, usuário existente, apenas exemplos.
                     </p>
                 </div>
 
-                <div class="form flex-col">
-                    <h1 class="font-title-black title-login">Login</h1>
+                <div className="form flex-col">
+                    <h1 className="font-title-black title-login">Login</h1>
 
-                    <div class="input">
-                        <label for="input-email" class="email font-inter-black">Email:</label>
-                        <input id="input-email" type="text" class="input-text"/>
+                    <div className="input">
+                        <label for="input-email" className="email font-inter-black">Email:</label>
+                        <input 
+                            id="input-email" 
+                            onChange={handleInputEmailChange}
+                            value={inputEmail}
+                            type="text" 
+                            className="input-text"
+                        />
                     </div>
-                    <div class="input">
-                        <label for="input-senha" class="senha font-inter-black">Senha:</label>
-                        <input id="input-senha" type="password" class="input-text"/>
+                    <div className="input">
+                        <label for="input-senha" className="senha font-inter-black">Senha:</label>
+                        <input 
+                            id="input-senha"
+                            onChange={handleInputPasswordChange}
+                            value={inputPassword}
+                            type="password" 
+                            className="input-text"
+                        />
                     </div>
-                    <input type="button" name="button" class="botao" value="Confirmar"/>
+                    <input 
+                        type="button"
+                        onClick={handleClickLogin}
+                        name="button" 
+                        className="botao" 
+                        value="Confirmar"
+                    />
                     <div id="without-register" onClick={handleRegister}>Ainda não possui cadastro?</div>
                 </div>
             </div>
--- app/src/Pages/Register.js
+++ app/src/Pages/Register.js
@@ -6,7 +6,7 @@ import "./css/Register.css"
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 
-const Login = () => {
+const Login = ({ dataUsers }) => {
 
     const navigate = useNavigate();
     const handleLogin = () => {
@@ -17,33 +17,33 @@ const Login = () => {
         <>
             <Header />
 
-            <div class="register-frame content">
-                <div class="error-message error-message-hide">
-                    <p class="error-message-text">
+            <div className="register-frame content">
+                <div className="error-message error-message-hide">
+                    <p className="error-message-text">
                         Possível mensagem de erro, errou a senha, usuário existente, apenas exemplos.
                     </p>
                 </div>
 
-                <div class="form flex-col">
-                    <h1 class="font-title-black title-register">Cadastre-se</h1>
+                <div className="form flex-col">
+                    <h1 className="font-title-black title-register">Cadastre-se</h1>
 
-                    <div class="input">
-                        <label for="input-nome" class="nome font-inter-black">Nome:</label>
-                        <input id="input-nome" type="text" class="input-text"/>
+                    <div className="input">
+                        <label for="input-nome" className="nome font-inter-black">Nome:</label>
+                        <input id="input-nome" type="text" className="input-text"/>
                     </div>
-                    <div class="input">
-                        <label for="input-email" class="email font-inter-black">Email:</label>
-                        <input id="input-email" type="text" class="input-text"/>
+                    <div className="input">
+                        <label for="input-email" className="email font-inter-black">Email:</label>
+                        <input id="input-email" type="text" className="input-text"/>
                     </div>
-                    <div class="input">
-                        <label for="input-senha" class="senha font-inter-black">Senha:</label>
-                        <input id="input-senha" type="password" class="input-text"/>
+                    <div className="input">
+                        <label for="input-senha" className="senha font-inter-black">Senha:</label>
+                        <input id="input-senha" type="password" className="input-text"/>
                     </div>
-                    <div class="input">
-                        <label for="input-confirmar-senha" class="confirmar-senha font-inter-black">Confirmar senha:</label>
-                        <input id="input-confirmar-senha" type="password" class="input-text"/>
+                    <div className="input">
+                        <label for="input-confirmar-senha" className="confirmar-senha font-inter-black">Confirmar senha:</label>
+                        <input id="input-confirmar-senha" type="password" className="input-text"/>
                     </div>
-                    <input type="button" name="button" class="botao" value="Confirmar"/>
+                    <input type="button" name="button" className="botao" value="Confirmar"/>
                     <div id="with-register" onClick={handleLogin}>Já possui cadastro?</div>
                 </div>
             </div>
